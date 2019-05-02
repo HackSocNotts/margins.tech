808996c1f9fa1347a893d1a9db5a77d34bc52ea1
Unknown <a.v.spors@gmail.com>
2019-05-02 16:11:33 +0100

    update website (ticket links!)

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 //Changing background image (on click)
-function changeBackground() 
+function changeBackground()
 {
   //Array containing background images
-  var images = ['https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2F05.png?1553448669278',
-                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2F04.png?1553448669387',
-                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2F03.png?1553448669498',
-                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2F02.png?1553448670491',
-                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2F01.png?1553448670952'];
-  
+  var images = ['https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg1.jpg?1553450735132',
+                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg-purple-fur-2.jpg?1556544635996',
+                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg-pink-fur-2.jpg?1556544636848',
+                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg-042ade90-dc51-43a7-96ec-9038b2aed19d%20(copy).jpeg?1556544636986',
+                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg-pink-sweater-2.jpg?1556544638021',
+                'https://cdn.glitch.com/b68166ed-17f0-40b5-82ff-59c7bd7d2514%2Fbg-pink-sweater-2-2.jpg?1556544638489',];
+
   //Generating a random image index
-  var randomNumber = Math.floor(Math.random() * images.length);
-  
-  //Setting background image to be the random index image 
-  document.body.style.backgroundImage = 'url(' + images[randomNumber] + ')';
-  document.body.style.backgroundColor = "#fa82e8";
+  //var randomNumber = Math.floor(Math.random() * images.length);
+
+  let bgDiv = document.querySelector('.js-bg');
+
+  //Setting background image to be the random index image
+  bgDiv.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+  document.body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
 }
+
+changeBackground();
